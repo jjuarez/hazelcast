a4f9599e4264bb13e74165ac1f70c8ffe4e4093a
Javier Juarez <javier.juarez@fon.com>
2018-03-17 19:38:58 +0100

    Bad dependency, the order is the opposite

--- manifests/config.pp
+++ manifests/config.pp
@@ -8,13 +8,13 @@ class hazelcast::config inherits hazelcast {
     group => $hazelcast::group,
   }
 
-  file { "${::halezcast::config_dir}/hazelcast.xml":
+  file { $::hazelcast::config_dir:
+    ensure => directory,
+    mode   => '0640',
+  }
+  -> file { "${::halezcast::config_dir}/hazelcast.xml":
     mode    => '0640',
     content => epp("${module_name}/hazelcast.xml.epp", { }),
     require => [File[$::hazelcast::install_dir], Package['java']], # TODO: I don't like this dependency at all, but
-  }
-  -> file { $::hazelcast::config_dir:
-    ensure => directory,
-    mode   => '0640',
   }
 }
