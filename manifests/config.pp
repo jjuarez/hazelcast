5d1b717a8612ca213ce986c40288217e3b981b84
Javier Juarez <javier.juarez@fon.com>
2018-03-18 13:08:49 +0100

    Changes in the mode of handle the module variables

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,22 +3,22 @@
 #
 class hazelcast::config inherits hazelcast {
 
-  file { $::hazelcast::config_dir:
+  file { $hazelcast::config_dir:
     ensure => directory,
     mode   => '0640',
   }
 
-  file { "$::hazelcast::config_dir/hazelcast.conf":
+  file { "${hazelcast::config_dir}/hazelcast.conf":
     ensure  => present,
     mode    => '0750',
     content => epp("${module_name}/hazelcast.conf.epp", { }),
-    require => File[$::hazelcast::config_dir]
+    require => File[$hazelcast::config_dir]
   }
 
-  file { "${::halezcast::config_dir}/hazelcast.xml":
+  file { "${halezcast::config_dir}/hazelcast.xml":
     ensure  => present,
     mode    => '0640',
     content => epp("${module_name}/hazelcast.xml.epp", { }),
-    require => [File[$::hazelcast::install_dir], File[$::hazelcast::config_dir]]
+    require => [File[$hazelcast::install_dir], File[$hazelcast::config_dir]]
   }
 }
