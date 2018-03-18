4017d53a8d44932886ac160df3d4a80ca9476850
Javier Juarez <javier.juarez@fon.com>
2018-03-18 17:02:28 +0100

    Simplifying the systemd work

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,22 +2,12 @@
 # = class: hazelcast::service - The class that handles the hazelcast service
 #
 class hazelcast::service inherits hazelcast {
-  include ::systemd::systemctl::daemon_reload
 
-  $systemd_unit_file = '/lib/systemd/system/hazelcast.service'
-
-  file { $systemd_unit_file:
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
+  ::system::unit_file { 'hazelcast.service'
     content => epp("${module_name}/hazelcast.service.epp", { }),
   }
-
-  service { $::hazelcast::service_name:
+  ~> service { $::hazelcast::service_name:
     ensure     => $::hazelcast::service_ensure,
-    hasrestart => true,
-    hasstatus  => true,
-    subscribe  => File[$systemd_unit_file],
+    subscribe  => File['/lib/systemd/system/hazelcast.service'],
   }
 }
